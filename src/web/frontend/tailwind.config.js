727a6757dc5050e188cc688951bd256ddd6b75e1
Daniel Müller <daniel.mueller@uni-muenster.de>
2023-08-02 15:49:39 +0200

    fix Tailwind config

--- src/web/frontend/tailwind.config.js
+++ src/web/frontend/tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [],
-    purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}", "../../common/web/**/*.{vue,js,ts,jsx,tsx}"],
+    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}", "../../common/web/**/*.{vue,js,ts,jsx,tsx}"],
     theme: {
         extend: {},
     },
