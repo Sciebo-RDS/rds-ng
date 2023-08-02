6fb5f46b7b5a4a4bdf545bc87a49dfbb4dee1c87
Daniel Müller <daniel.mueller@uni-muenster.de>
2023-08-02 14:42:10 +0200

    add PrimeVue and Tailwind

--- src/web/frontend/src/App.vue
+++ src/web/frontend/src/App.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
+
 </script>
 
 <template>
     <header>
         <img alt="Vue logo" class="logo" src="@common/assets/img/rds-octopus-bl.svg" width="250" height="250"/>
     </header>
+    <Button label="Submit" class="bg-red-500 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded" unstyled/>
+    <h1 class="text-3xl font-bold underline bg-amber-200">
+        Hello world!
+    </h1>
 </template>
--- src/web/frontend/src/main.ts
+++ src/web/frontend/src/main.ts
@@ -1,10 +1,20 @@
-import {createApp} from 'vue'
-import {createPinia} from 'pinia'
+import '@common/assets/styles/tailwind-init.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import PrimeVue from 'primevue/config'
 
 import App from './App.vue'
+import Button from "primevue/button"
+import "primevue/resources/themes/lara-light-indigo/theme.css"
 
 const app = createApp(App)
 
 app.use(createPinia())
+
+app.use(PrimeVue)
+
+app.component('Button', Button);
 
 app.mount('#app')
