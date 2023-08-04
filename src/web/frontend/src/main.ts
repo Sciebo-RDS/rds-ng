114b0c532159a6883501ec566b58adbe285074bd
Daniel Müller <daniel.mueller@uni-muenster.de>
2023-08-04 14:11:10 +0200

    remove superfluous imports

--- src/web/frontend/src/main.ts
+++ src/web/frontend/src/main.ts
@@ -1,8 +1,4 @@
-import "@common/assets/styles/tailwind-init.css"
-
 import App from "./App.vue"
-import "primevue/resources/themes/lara-light-indigo/theme.css"
-
 import createApplication from "@common/lib/app/Application";
 
 const app = createApplication(App);
