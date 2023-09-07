2e957e5ff0dc9c48021031b247b4719910db5d03
Daniel Müller <daniel.mueller@uni-muenster.de>
2023-09-07 17:21:40 +0200

    initial frontend work

---
+++ src/frontend/src/ui/Frontend.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { onMounted } from "vue"
+
+import { WebComponent } from "@common/component/WebComponent";
+
+const comp = WebComponent.inject();
+
+// Always run our web component when the main Vue component has been mounted
+onMounted(() => {
+    comp.run();
+});
+</script>
+
+<template>
+    <router-view></router-view>
+</template>
