04134f9bef170128f28340b2de1d33524456b7de
Daniel Müller <daniel.mueller@uni-muenster.de>
2023-09-18 17:21:44 +0200

    some header playing

--- src/frontend/src/ui/projectlist/Header.vue
+++ src/frontend/src/ui/projectlist/Header.vue
@@ -2,8 +2,33 @@
 </script>
 
 <template>
+    <!--
+    <div class="grid grid-rows-2 grid-cols-[min-content_1fr] r-primary-bg r-primary-text">
+        <div class="row-span-2">
+            <a href="https://www.research-data-services.org" target="_blank">
+                <img src="@assets/img/rds-octopus-bl.svg" alt="RDS Logo" class="max-h-full p-1.5" title="Visit the RDS website">
+            </a>
+        </div>
+        <div class="font-bold">
+            Donald T. Rump
+        </div>
+        <div>
+            3 Projects
+        </div>
+    </div>
+    -->
     <div class="r-primary-bg r-primary-text">
-        Project List Header
+        <a href="https://www.research-data-services.org" target="_blank">
+            <img src="@assets/img/rds-octopus-bl.svg" alt="RDS Logo" class="max-h-full p-1.5 float-left" title="Visit the RDS website">
+        </a>
+        <div class="grid grid-rows-2 h-full content-center items-center pl-2 py-3">
+            <div class="font-bold">
+                Donald T. Rump
+            </div>
+            <div>
+                3 Projects
+            </div>
+        </div>
     </div>
 </template>
 
