33980135d0191865f275355779b6f26d05a8d659
Daniel Müller <daniel.mueller@uni-muenster.de>
2023-09-19 13:30:14 +0200

    more header playing

--- src/frontend/src/ui/projectlist/Header.vue
+++ src/frontend/src/ui/projectlist/Header.vue
@@ -1,36 +1,32 @@
 <script setup lang="ts">
+import Button from "primevue/button";
 </script>
 
 <template>
-    <!--
-    <div class="grid grid-rows-2 grid-cols-[min-content_1fr] r-primary-bg r-primary-text">
+    <div class="grid grid-rows-2 grid-cols-[min-content_1fr_min-content] grid-flow-col gap-x-2 content-center items-center r-primary-bg r-primary-text">
         <div class="row-span-2">
             <a href="https://www.research-data-services.org" target="_blank">
-                <img src="@assets/img/rds-octopus-bl.svg" alt="RDS Logo" class="max-h-full p-1.5" title="Visit the RDS website">
+                <img id="logo" src="@assets/img/rds-octopus-wh.svg" alt="RDS Logo" class="p-1.5" title="Visit the RDS website">
             </a>
         </div>
-        <div class="font-bold">
+        <div class="font-bold pt-3">
             Donald T. Rump
         </div>
-        <div>
+        <div class="italic pb-3">
             3 Projects
         </div>
-    </div>
-    -->
-    <div class="r-primary-bg r-primary-text">
-        <a href="https://www.research-data-services.org" target="_blank">
-            <img src="@assets/img/rds-octopus-bl.svg" alt="RDS Logo" class="max-h-full p-1.5 float-left" title="Visit the RDS website">
-        </a>
-        <div class="grid grid-rows-2 h-full content-center items-center pl-2 py-3">
-            <div class="font-bold">
-                Donald T. Rump
-            </div>
-            <div>
-                3 Projects
-            </div>
+        <div class="row-span-2 pr-2">
+            <Button plain rounded aria-label="Settings">
+                <template #icon>
+                    <span class="material-icons-outlined r-primary-text" style="font-size: 40px;">settings</span>
+                </template>
+            </Button>
         </div>
     </div>
 </template>
 
 <style scoped lang="scss">
+#logo {
+    min-width: 5rem;
+}
 </style>
