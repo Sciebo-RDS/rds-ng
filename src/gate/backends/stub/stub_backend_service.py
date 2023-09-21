13347e1e7539008f51b6cb18ccacc810e1945b51
Daniel Müller <daniel.mueller@uni-muenster.de>
2023-09-21 12:28:21 +0200

    initial stub data

--- src/gate/backends/stub/stub_backend_service.py
+++ src/gate/backends/stub/stub_backend_service.py
@@ -1,3 +1,5 @@
+import time
+
 from common.py.component import BackendComponent
 from common.py.services import Service
 
@@ -26,4 +28,35 @@ def create_stub_backend_service(comp: BackendComponent) -> Service:
             ctx.message_builder, msg, projects=ctx.storage_pool.project_storage.list()
         )
 
+    # Add some initial data to the in-memory storage
+    _fill_stub_data()
+
     return svc
+
+
+def _fill_stub_data():
+    from ...data.storage.memory import MemoryStoragePool
+
+    from common.py.data.entities import Project
+
+    pool = (
+        MemoryStoragePool()
+    )  # The memory storage pool uses shared data objects, so we can fill them using a new instance
+
+    # -- Projects
+    pool.project_storage.add(
+        Project(
+            project_id=1000,
+            creation_time=time.time(),
+            name="Our first project",
+            description="This is our first attempt to create a project",
+        )
+    )
+    pool.project_storage.add(
+        Project(
+            project_id=1001,
+            creation_time=time.time(),
+            name="Top-secret experiments",
+            description="If you read this, the FBI is already on their way to you!",
+        )
+    )
