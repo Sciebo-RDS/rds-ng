c63d2a0e6133b66dcdcdd621caa6ba266c4501e0
Daniel Müller <daniel.mueller@uni-muenster.de>
2023-09-25 10:41:31 +0200

    make in-mem storage thread-safe

--- src/common/py/data/storage/project_storage.py
+++ src/common/py/data/storage/project_storage.py
@@ -8,3 +8,8 @@ class ProjectStorage(Storage[Project, ProjectID], abc.ABC):
     """
     Storage for projects.
     """
+
+    def __init__(self):
+        import threading
+
+        self._lock = threading.RLock()
--- src/gate/data/storage/memory/memory_project_storage.py
+++ src/gate/data/storage/memory/memory_project_storage.py
@@ -12,22 +12,26 @@ class MemoryProjectStorage(ProjectStorage):
     _projects: typing.Dict[ProjectID, Project] = {}
 
     def add(self, project: Project) -> None:
-        MemoryProjectStorage._projects[project.project_id] = project
+        with self._lock:
+            MemoryProjectStorage._projects[project.project_id] = project
 
     def remove(self, project: Project) -> None:
-        try:
-            del MemoryProjectStorage._projects[project.project_id]
-        except:  # pylint: disable=bare-except
-            from common.py.data.storage import StorageException
+        with self._lock:
+            try:
+                del MemoryProjectStorage._projects[project.project_id]
+            except:  # pylint: disable=bare-except
+                from common.py.data.storage import StorageException
 
-            raise StorageException(
-                f"A project with ID {project.project_id} was not found"
-            )
+                raise StorageException(
+                    f"A project with ID {project.project_id} was not found"
+                )
 
     def get(self, key: ProjectID) -> Project | None:
-        if key in MemoryProjectStorage._projects:
-            return MemoryProjectStorage._projects[key]
-        return None
+        with self._lock:
+            if key in MemoryProjectStorage._projects:
+                return MemoryProjectStorage._projects[key]
+            return None
 
     def list(self) -> typing.List[Project]:
-        return list(MemoryProjectStorage._projects.values())
+        with self._lock:
+            return list(MemoryProjectStorage._projects.values())
