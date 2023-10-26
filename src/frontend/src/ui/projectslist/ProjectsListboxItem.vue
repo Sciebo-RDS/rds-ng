1cae6b26a6eaecb70d8e2b6a86db41b321fa9ca4
Daniel Müller <daniel.mueller@uni-muenster.de>
2023-10-26 14:14:22 +0200

    keep kebab button visible if pressed

--- src/frontend/src/ui/projectslist/ProjectsListboxItem.vue
+++ src/frontend/src/ui/projectslist/ProjectsListboxItem.vue
@@ -9,8 +9,9 @@ const state = toRefs(props);
 const project = state.project;
 const isSelected = state.isSelected;
 
-const menu = ref();
-const items = ref([
+const editMenu = ref();
+const editMenuShown = ref(false);
+const editMenuItems = ref([
     {
         label: 'Options',
         items: [
@@ -30,8 +31,8 @@ const items = ref([
     }
 ]);
 
-const toggle = (event) => {
-    menu.value.toggle(event);
+const showEditMenu = (event) => {
+    editMenu.value.toggle(event);
 };
 </script>
 
@@ -39,12 +40,12 @@ const toggle = (event) => {
     <div class="grid grid-rows-[auto_auto] grid-cols-[1fr_min-content] gap-0 h-24 place-content-start group">
         <div class="r-text-caption-big h-8 truncate" :title="project.name">{{ project.name }}</div>
         <div class="row-span-2 pl-1">
-            <Button text rounded size="small" aria-label="Options" title="More options" class="invisible group-hover:visible" @click="toggle">
+            <Button text rounded size="small" aria-label="Options" title="More options" :class="{ 'invisible': !editMenuShown, 'group-hover:visible': true }" @click="showEditMenu">
                 <template #icon>
                     <span class="material-icons-outlined" :class="[isSelected ? 'r-primary-text' : 'r-text']" style="font-size: 32px;">more_vert</span>
                 </template>
             </Button>
-            <Menu ref="menu" id="overlay_menu" :model="items" :popup="true"/>
+            <Menu ref="editMenu" :model="editMenuItems" :popup="true" @focus="editMenuShown=true" @blur="editMenuShown=false"/>
         </div>
 
         <div id="project-description" class="overflow-hidden line-clamp" :title="project.description">{{ project.description }}</div>
