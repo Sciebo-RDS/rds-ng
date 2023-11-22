d1d19282982d1d8e7508a306d3a5c8975b1024e8
Daniel Müller <daniel.mueller@uni-muenster.de>
2023-11-22 16:41:18 +0100

    small visual changes

--- src/common/web/ui/dialogs/ExtendedDialogFooter.vue
+++ src/common/web/ui/dialogs/ExtendedDialogFooter.vue
@@ -8,7 +8,7 @@ const { acceptDialog, rejectDialog, dialogData } = extendedDialogTools();
 
 <template>
     <div class="grid grid-rows-1 grid-cols-[1fr_min-content_min-content] place-items-baseline">
-        <small v-if="dialogData.options.showMandatoryHint" class="p-error font-bold">* denotes mandatory fields</small>
+        <small v-if="dialogData.options.showMandatoryHint" class="p-error italic"><span class="font-bold">*</span> denotes mandatory fields</small>
         <small v-else>&nbsp;</small>
         <Button v-if="dialogData.options.hasAcceptButton" :label="dialogData.options.acceptLabel || 'OK'" :icon="dialogData.options.acceptIcon" size="small" @click="acceptDialog" />
         <Button v-if="dialogData.options.hasRejectButton" :label="dialogData.options.rejectLabel || 'Cancel'" :icon="dialogData.options.rejectIcon" size="small" @click="rejectDialog" />
