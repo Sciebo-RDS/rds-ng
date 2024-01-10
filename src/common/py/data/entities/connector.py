d8b3ac5128dd7a34b58ea4f8b8e7d5a121aa2c31
Daniel Müller <daniel.mueller@uni-muenster.de>
2024-01-10 12:31:50 +0100

    connector entity

---
+++ src/common/py/data/entities/connector.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+
+from dataclasses_json import dataclass_json
+
+from ...utils import UnitID
+
+ConnectorID = UnitID
+
+
+@dataclass_json
+@dataclass(frozen=True, kw_only=True)
+class Connector:
+    """
+    Data for a single **Connector**.
+
+    Attributes:
+        connector_id: The unique connector identifier.
+        name: The name of the connector.
+        description: An optional connector description.
+    """
+
+    connector_id: ConnectorID
+
+    name: str
+    description: str
