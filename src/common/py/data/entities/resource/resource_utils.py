3685e6bbd1acc7ebfa5dcdb6c123803fe09e9d55
Daniel Müller <daniel.mueller@uni-muenster.de>
2024-01-26 16:52:17 +0100

    extend file listing options

--- src/common/py/data/entities/resource/resource_utils.py
+++ src/common/py/data/entities/resource/resource_utils.py
@@ -3,12 +3,20 @@ import pathlib
 from .resources_list import ResourcesList, ResourceFiles, ResourceFolders
 
 
-def resources_list_from_syspath(path: str, *, recursive: bool = True) -> ResourcesList:
+def resources_list_from_syspath(
+    path: str,
+    *,
+    include_files: bool = True,
+    include_folders: bool = True,
+    recursive: bool = True
+) -> ResourcesList:
     """
     Generates a *ResourcesList* from a system path.
 
     Args:
         path: The root path.
+        include_files: Whether to list files.
+        include_folders: Whether to list folders (if this is set to false, no recursion will occur independent of `recursive`).
         recursive: Whether to recursively process all sub-folders as well.
 
     Returns:
@@ -20,11 +28,11 @@ def resources_list_from_syspath(path: str, *, recursive: bool = True) -> Resourc
         files: ResourceFiles = []
 
         for child_path in root_path.iterdir():
-            if child_path.is_dir():
+            if child_path.is_dir() and include_folders:
                 folders[str(child_path)] = (
                     _process_path(child_path) if recursive else None
                 )
-            elif child_path.is_file():
+            elif child_path.is_file() and include_files:
                 files.append(str(child_path))
 
         return ResourcesList(resource=str(root_path), folders=folders, files=files)
