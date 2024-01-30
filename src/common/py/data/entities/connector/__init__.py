a7d53c7083266474cbd31c90caf93ee7bafd76a5
Daniel Müller <daniel.mueller@uni-muenster.de>
2024-01-30 12:53:09 +0100

    project options for active connections

--- src/common/py/data/entities/connector/__init__.py
+++ src/common/py/data/entities/connector/__init__.py
@@ -1,4 +1,4 @@
 from .connector import Connector, ConnectorID
-from .connector_instance import ConnectorInstance
+from .connector_instance import ConnectorInstance, ConnectorInstanceID
 
 from .connector_utils import find_connector_by_id
--- src/common/web/data/entities/project/ProjectOptions.ts
+++ src/common/web/data/entities/project/ProjectOptions.ts
@@ -1,5 +1,6 @@
 import { Type } from "class-transformer";
 
+import { type ConnectorInstanceID } from "../connector/ConnectorInstance";
 import { type ProjectFeatureID } from "./features/ProjectFeature";
 
 /**
@@ -11,6 +12,8 @@ export type UIOptions = Record<string, any>;
  * Class holding all options of a **Project**.
  *
  * @param optional_features - A list of all user-enabled optional features.
+ * @param use_all_connector_instances - Whether all available connector instances should be used.
+ * @param active_connector_instances - List of connector instances to use for the project (if *use_all_connector_instances* is false).
  * @param ui - Arbitrary UI options.
  */
 export class ProjectOptions {
@@ -18,10 +21,18 @@ export class ProjectOptions {
     @Type(() => String)
     public readonly optional_features: ProjectFeatureID[];
 
+    use_all_connector_instances: boolean;
+    // @ts-ignore
+    @Type(() => String)
+    active_connector_instances: ConnectorInstanceID[];
+
     public readonly ui: UIOptions;
 
-    public constructor(optionalFeatures: ProjectFeatureID[] = [], uiOptions: UIOptions = {}) {
+    public constructor(optionalFeatures: ProjectFeatureID[] = [], useAllConnectors: boolean = true, activeConnectorInstances: ConnectorInstanceID[] = [], uiOptions: UIOptions = {}) {
         this.optional_features = optionalFeatures;
+
+        this.use_all_connector_instances = useAllConnectors;
+        this.active_connector_instances = activeConnectorInstances;
 
         this.ui = uiOptions;
     }
