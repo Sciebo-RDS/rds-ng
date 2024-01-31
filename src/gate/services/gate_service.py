bcb8d7a0cead4e7d7ea4a8099d364f54f54a1c56
Daniel Müller <daniel.mueller@uni-muenster.de>
2024-01-31 16:34:21 +0100

    clear session storage on timeout

--- src/gate/services/gate_service.py
+++ src/gate/services/gate_service.py
@@ -17,10 +17,15 @@ def create_gate_service(comp: BackendComponent) -> Service:
 
     """
 
-    svc = comp.create_service("Gate service")
+    svc = comp.create_service("Gate service", context_type=GateServiceContext)
 
     @svc.message_handler(ServerTimeoutEvent)
     def server_timeout(msg: ServerTimeoutEvent, ctx: GateServiceContext) -> None:
-        ctx.logger.debug("A client timed out", scope="gate", client=msg.comp_id)
+        ctx.logger.debug(
+            "Clearing session data due to timeout", scope="gate", session=msg.comp_id
+        )
+
+        # A timeout automatically clears the session data of that client
+        ctx.session_storage.clear_data(msg.comp_id)
 
     return svc
