ee2fe727a59454c74c06bfa33c53f1110b3dbabb
Daniel Müller <daniel.mueller@uni-muenster.de>
2024-02-21 17:41:22 +0100

    remove pdf due to errors

--- src/common/web/ui/components/propertyeditor/exporters/pdf/_PdfExporter.ts
+++ src/common/web/ui/components/propertyeditor/exporters/pdf/_PdfExporter.ts
@@ -1,11 +1,12 @@
 // @ts-nocheck
-import pdfMake from "pdfmake/build/pdfmake";
-import pdfFonts from "pdfmake/build/vfs_fonts";
+//import pdfMake from "pdfmake/build/pdfmake";
+//import pdfFonts from "pdfmake/build/vfs_fonts";
 
 export class PdfExporterExport {
     static downloadPdf(dD: any, title: string): void {
-        (<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
+        /*(<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
         const fileName = `DMP_${!!title ? title.replaceAll(" ", "_") : "unnamed"}_${new Date().toLocaleDateString()}.pdf`;
         pdfMake.createPdf(dD).download(fileName);
+         */
     }
 }
