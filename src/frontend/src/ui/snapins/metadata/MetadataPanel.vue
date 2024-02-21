500c6d96eed92b01579e228e3b7f20bf22bb4750
Daniel Müller <daniel.mueller@uni-muenster.de>
2024-02-21 13:11:39 +0100

    connector metadata profiles

--- src/frontend/src/ui/snapins/metadata/MetadataPanel.vue
+++ src/frontend/src/ui/snapins/metadata/MetadataPanel.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { storeToRefs } from "pinia";
 import { reactive, toRefs } from "vue";
 
 import logging from "@common/core/logging/Logging";
@@ -6,7 +7,6 @@ import { Project } from "@common/data/entities/project/Project";
 import { MetadataFeature, type ProjectMetadata } from "@common/data/entities/project/features/MetadataFeature";
 import { testProfile } from "@common/ui/components/propertyeditor/DummyData";
 import { dataCite } from "@common/ui/components/propertyeditor/profiles/datacite";
-import { osf } from "@common/ui/components/propertyeditor/profiles/osf";
 import { MetadataController } from "@common/ui/components/propertyeditor/PropertyController";
 import { PersistedSet, PropertySet } from "@common/ui/components/propertyeditor/PropertySet";
 import { extractPersistedSetFromArray } from "@common/ui/components/propertyeditor/utils/PropertyEditorUtils";
@@ -14,6 +14,8 @@ import { extractPersistedSetFromArray } from "@common/ui/components/propertyedit
 import PropertyEditor from "@common/ui/components/propertyeditor/PropertyEditor.vue";
 
 import { FrontendComponent } from "@/component/FrontendComponent";
+import { useConnectorsStore } from "@/data/stores/ConnectorsStore";
+import { useUserStore } from "@/data/stores/UserStore";
 import { UpdateProjectFeaturesAction } from "@/ui/actions/project/UpdateProjectFeaturesAction";
 
 const comp = FrontendComponent.inject();
@@ -24,12 +26,27 @@ const props = defineProps({
     }
 });
 const { project } = toRefs(props);
+const consStore = useConnectorsStore();
+const userStore = useUserStore();
+const { connectors } = storeToRefs(consStore);
+const { userSettings } = storeToRefs(userStore);
 
 // TODO: Testing data only
+const mergeSets: PropertySet[] = [];
+
+connectors.value.forEach((connector) => {
+    const metadataProfile = connector.metadata_profile;
+    if (metadataProfile.hasOwnProperty("profile_id")) {
+        mergeSets.push(new PropertySet(
+            connector.metadata_profile,
+            extractPersistedSetFromArray(project!.value.features.metadata.metadata, metadataProfile["profile_id"])
+        ));
+    }
+});
+
 const baseSet = new PropertySet(dataCite, extractPersistedSetFromArray(project!.value.features.metadata.metadata, dataCite.profile_id));
-const mergeSet = [new PropertySet(osf, extractPersistedSetFromArray(project!.value.features.metadata.metadata, osf.profile_id))];
 const profiles: PropertySet[] = [new PropertySet(testProfile, extractPersistedSetFromArray(project!.value.features.metadata.metadata, testProfile.profile_id))];
-const controller = reactive(new MetadataController(baseSet, mergeSet, profiles)) as MetadataController;
+const controller = reactive(new MetadataController(baseSet, mergeSets, profiles)) as MetadataController;
 
 const handleMetadataUpdate: Function = (data: PersistedSet) => {
     const metadata = data as ProjectMetadata;
