387e08b5128c68c4670feb2b0ce340ef4acba29e
Daniel Müller <daniel.mueller@uni-muenster.de>
2024-03-05 14:20:54 +0100

    work on using JWT

--- src/frontend/src/settings/FrontendSettingIDs.ts
+++ src/frontend/src/settings/FrontendSettingIDs.ts
@@ -9,5 +9,7 @@ import { SettingID } from "@common/utils/config/SettingID";
 export class FrontendSettingIDs {
     public static readonly RegularCommandTimeout = new SettingID("frontend", "regular_command_timeout");
 
+    public static readonly PublicKeyURL = new SettingID("frontend", "public_key_url"); // TODO: Temporary
+
     public static readonly UseLoginPage = new SettingID("frontend", "use_login_page");
 }
--- src/frontend/src/ui/Frontend.vue
+++ src/frontend/src/ui/Frontend.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
+import { useUrlSearchParams } from "@vueuse/core";
+import { useAxios } from "@vueuse/integrations/useAxios";
+import * as jwt from "jsonwebtoken";
 import { storeToRefs } from "pinia";
-import { computed } from "vue";
+import { computed, onMounted, unref, watch } from "vue";
 
 import { FrontendComponent } from "@/component/FrontendComponent";
 import { useUserStore } from "@/data/stores/UserStore";
@@ -14,6 +17,23 @@ const { userToken } = storeToRefs(userStore);
 
 // If enabled, show the dummy login page to get a user ID token
 const showLoginPage = computed(() => comp.data.config.value<boolean>(FrontendSettingIDs.UseLoginPage) && !userToken.value);
+
+onMounted(() => {
+    const pubKeyURL = comp.data.config.value<string>(FrontendSettingIDs.PublicKeyURL);
+    console.log(pubKeyURL);
+
+    const { data, isFinished } = useAxios(pubKeyURL);
+
+    watch(isFinished, (f) => {
+        if (f) {
+            const key = JSON.parse(unref(data)["public-key"]);
+            console.log(key);
+            const queryParams = useUrlSearchParams("history");
+            console.log(queryParams["user-token"]);
+            jwt.verify(queryParams["user-token"], key);
+        }
+    });
+});
 
 </script>
 
