6260525698cfe9cbf2079f45b28e793539f1e1f8
Daniel Müller <daniel.mueller@uni-muenster.de>
2024-03-07 14:45:52 +0100

    fix Vue warnings

--- src/frontend/src/ui/Frontend.vue
+++ src/frontend/src/ui/Frontend.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
-import { computed, onMounted, onUnmounted, shallowRef } from "vue";
+import { onMounted, onUnmounted, ref, shallowRef, watch } from "vue";
 import { RouterView } from "vue-router";
 
 import { FrontendComponent } from "@/component/FrontendComponent";
 
 const comp = FrontendComponent.inject();
 const authScheme = shallowRef(comp.authenticationScheme);
-const isAuthenticated = computed(() => authScheme.value.isAuthenticated);
+const isAuthenticated = ref(authScheme.value.isAuthenticated);
+
+// Circumvent Vue warnings arising from using AuthorizationScheme.isAuthenticated directly
+watch(() => authScheme.value.isAuthenticated, (authenticated) => {
+    isAuthenticated.value = authenticated;
+});
 
 onMounted(() => {
     // The app has been loaded; notify the authentication scheme about this
