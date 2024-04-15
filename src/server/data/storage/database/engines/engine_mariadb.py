c921b0a49225b48baf780f12b5d57fb87524f148
Daniel Müller <daniel.mueller@uni-muenster.de>
2024-04-15 12:47:11 +0200

    add DB driver packages

--- src/server/data/storage/database/engines/engine_mariadb.py
+++ src/server/data/storage/database/engines/engine_mariadb.py
@@ -5,5 +5,7 @@ from .engine_utils import format_database_url
 def get_engine_parameters_mariadb(
     host: str, port: int, database: str, user: str, password: str
 ) -> EngineParameters:
-    db_url = "mariadb://" + format_database_url(host, port, database, user, password)
+    db_url = "mariadb+mariadbconnector://" + format_database_url(
+        host, port, database, user, password
+    )
     return db_url, {}, {}
--- src/server/data/storage/database/engines/engine_mysql.py
+++ src/server/data/storage/database/engines/engine_mysql.py
@@ -5,5 +5,7 @@ from .engine_utils import format_database_url
 def get_engine_parameters_mysql(
     host: str, port: int, database: str, user: str, password: str
 ) -> EngineParameters:
-    db_url = "mysql://" + format_database_url(host, port, database, user, password)
+    db_url = "mysql+mysqlconnector://" + format_database_url(
+        host, port, database, user, password
+    )
     return db_url, {}, {}
