308f731fcd0437c87edb50ea310d78cab92351af
Juri Hößelbarth <hoesselbarth@uni-muenster.de>
2024-04-17 14:36:45 +0200

    adds PropertyDialog for creating and editing properties

---
+++ src/common/web/ui/components/propertyeditor/PropertyDialog.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+import Breadcrumb from "primevue/breadcrumb";
+
+let path = ["propery", "sub property", "sub sub property"];
+let menuPath = ref();
+updatePath(path.length - 1);
+
+function updatePath(i: number) {
+    path = path.slice(0, i + 1);
+    menuPath.value = path.map((obj, i) => {
+        return {
+            label: obj,
+            command: () => {
+                updatePath(i);
+            }
+        };
+    });
+}
+</script>
+
+<template>
+    <div>
+        <Breadcrumb :model="menuPath" :pt="{ root: { class: '' }, label: { class: 'text-red-900 opacity-80 hover:opacity-100 capitalize' } }" />
+    </div>
+</template>
