eb729f77edb6f1ef5974d4f0950e9e53ba997d46
Daniel Müller <daniel.mueller@uni-muenster.de>
2024-04-19 15:06:23 +0200

    verify connectors

--- src/common/py/data/verifiers/connector/connector_verifier.py
+++ src/common/py/data/verifiers/connector/connector_verifier.py
@@ -12,10 +12,12 @@ class ConnectorVerifier(Verifier):
 
     def verify_create(self) -> None:
         self._verify_id()
+        self._verify_category()
         self._verify_name()
 
     def verify_update(self) -> None:
         self._verify_id()
+        self._verify_category()
         self._verify_name()
 
     def verify_delete(self) -> None:
@@ -24,6 +26,15 @@ class ConnectorVerifier(Verifier):
     def _verify_id(self) -> None:
         if self._connector.connector_id == "":
             raise VerificationException("Invalid connector ID")
+
+    def _verify_category(self) -> None:
+        from ...entities.connector.categories import ConnectorCategoriesCatalog
+
+        if (
+            self._connector.category == ""
+            or ConnectorCategoriesCatalog.find_item(self._connector.category) is None
+        ):
+            raise VerificationException("Invalid connector category")
 
     def _verify_name(self) -> None:
         if self._connector.name == "":
--- src/server/services/connectors_service.py
+++ src/server/services/connectors_service.py
@@ -3,6 +3,7 @@ import time
 from common.py.component import BackendComponent
 from common.py.core.logging import info, debug, warning
 from common.py.data.entities.connector import Connector
+from common.py.data.verifiers.connector import ConnectorVerifier
 from common.py.services import Service
 
 _MAX_CONNECTOR_AGE = (
@@ -54,25 +55,38 @@ def create_connectors_service(comp: BackendComponent) -> Service:
             announce_timestamp=time.time(),
         )
 
-        if (
-            ex_connector := ctx.storage_pool.connector_storage.get(msg.connector_id)
-        ) is not None:
-            from common.py.data.entities.connector import apply_connector_update
+        try:
+            if (
+                ex_connector := ctx.storage_pool.connector_storage.get(msg.connector_id)
+            ) is not None:
+                from common.py.data.entities.connector import apply_connector_update
 
-            apply_connector_update(ex_connector, connector)
-            debug(
-                "Connector updated",
+                ConnectorVerifier(connector).verify_update()
+
+                apply_connector_update(ex_connector, connector)
+                debug(
+                    "Connector updated",
+                    scope="connectors",
+                    id=msg.connector_id,
+                    name=msg.display_name,
+                )
+            else:
+                ConnectorVerifier(connector).verify_create()
+
+                ctx.storage_pool.connector_storage.add(connector)
+                debug(
+                    "Connector added",
+                    scope="connectors",
+                    id=msg.connector_id,
+                    name=msg.display_name,
+                )
+        except Exception as exc:  # pylint: disable=broad-exception-caught
+            warning(
+                "An invalid connector announce has been received",
                 scope="connectors",
                 id=msg.connector_id,
                 name=msg.display_name,
-            )
-        else:
-            ctx.storage_pool.connector_storage.add(connector)
-            debug(
-                "Connector added",
-                scope="connectors",
-                id=msg.connector_id,
-                name=msg.display_name,
+                error=str(exc),
             )
 
     @svc.message_handler(ComponentProcessEvent)
