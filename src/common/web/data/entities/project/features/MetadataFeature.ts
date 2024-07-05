faa646f2d04dac25950eb3d0dc5272c6d1750b5b
Daniel Müller <daniel.mueller@uni-muenster.de>
2024-07-05 17:02:54 +0200

    add shared metadata objects

--- src/common/web/data/entities/project/features/MetadataFeature.ts
+++ src/common/web/data/entities/project/features/MetadataFeature.ts
@@ -8,17 +8,26 @@ import { ProjectFeature, type ProjectFeatureID } from "./ProjectFeature";
 export type ProjectMetadata = Record<string, any>[];
 
 /**
+ * The metadata objects type.
+ *
+ * TODO: Use proper type
+ */
+export type MetadataObjects = Record<string, any>[];
+
+/**
  * Data class for the metadata project feature.
  */
 export class MetadataFeature extends ProjectFeature {
     public static readonly FeatureID: ProjectFeatureID = "metadata";
 
     public readonly metadata: ProjectMetadata;
+    public readonly shared_objects: MetadataObjects;
 
-    public constructor(metadata: ProjectMetadata = []) {
+    public constructor(metadata: ProjectMetadata = [], sharedObjects: MetadataObjects = []) {
         super();
 
         this.metadata = metadata;
+        this.shared_objects = sharedObjects;
     }
 
     public get featureID(): ProjectFeatureID {
