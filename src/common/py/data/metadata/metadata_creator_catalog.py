82afdbd504248290a6dbfa6ecbaaa39b93185d21
Daniel Müller <daniel.mueller@uni-muenster.de>
2024-10-22 10:48:13 +0200

    remove hardcoded stuff

--- src/common/py/data/metadata/metadata_creator_catalog.py
+++
@@ -1,9 +0,0 @@
-from common.py.data.metadata import MetadataCreator
-from common.py.utils.items_catalog import ItemsCatalog
-
-
-@ItemsCatalog.define()
-class MetadataCreatorCatalog(ItemsCatalog[MetadataCreator]):
-    """
-    Global catalog of connector metadata factories.
-    """
