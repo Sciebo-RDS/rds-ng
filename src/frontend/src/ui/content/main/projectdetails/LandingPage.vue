5e5fe0ede555e8aefeb309853d5e20dbaa029f83
Juri <hoesselbarth@uni-muenster.de>
2024-10-28 14:18:01 +0100

    Update LandingPage.vue

--- src/frontend/src/ui/content/main/projectdetails/LandingPage.vue
+++ src/frontend/src/ui/content/main/projectdetails/LandingPage.vue
@@ -5,84 +5,48 @@ import ExternalLink from "@common/ui/components/misc/ExternalLink.vue";
 </script>
 
 <template>
-    <div class="r-centered-grid">
-        <div class="w-2/3">
-            <div class="text-xl">
-                <strong class="text-2xl">Welcome to sciebo Research Data Services (sciebo RDS)</strong> – Your flexible solution for research data managing and
-                publishing. The services are designed to make data management effortless and collaborative, so you can focus on what matters most: your
-                research.
-            </div>
-            <div class="text-lg">Here's how we support you:</div>
-            <div>
-                <ul class="pl-6">
-                    <li class="flex items-center gap-1">
-                        &#x2022;
-                        <strong>Configure research processes -</strong> A research data tool in your cloud that flexibly adapts to your individual research
-                        workflows.
-                    </li>
-                    <li class="flex items-center gap-1">
-                        &#x2022;
-                        <strong>Project Workflow Integration -</strong> A research data tool that connects existing research data services according to your
-                        needs
-                    </li>
-                    <li class="flex items-center gap-1">
-                        &#x2022;
-                        <strong>User Control -</strong> Full control over your data, with clear options to manage access, set metadata and conditions for
-                        sharing.
-                    </li>
-                    <li class="flex items-center gap-1">
-                        &#x2022;
-                        <strong>Ease of Use -</strong> Intuitive services that integrate smoothly into your daily research activities, saving you time and
-                        effort.
-                    </li>
-                </ul>
-            </div>
-            <div class="pl-6">
-                Learn more:
-                <ExternalLink link="https://github.com/Sciebo-RDS" text="GitHub repository" />
-            </div>
-            <div class="pt-4 text-lg">To get started, simply follow these steps:</div>
-            <div>
-                <div class="flex items-center gap-4 pt-8 place-self-center">
-                    <Card :pt="{ root: 'bg-slate-100 text-center w-64 h-48', content: 'pt-1 place-self-center' }">
-                        <template #title>Initial setup</template>
-                        <template #content>
-                            <span class="!text-4xl material-icons-outlined mi-settings pb-2" />
-                            <p class="flex items-center gap-1 r-text-gray">Add repository connections</p>
-                        </template>
-                    </Card>
+    <div class="bg-slate-50">
+        <div class="content-center r-shade-text m-10">
+            <div class="grid grid-cols-2 min-w-full min-h-full place-content-center">
+                <div class="space-y-8">
+                    <div>
+                        <div class="text font-light uppercase">Sciebo Research Data Services</div>
+                        <div class="text-[2.7rem] font-black uppercase leading-none">Research data management, made easy</div>
+                    </div>
+                    <div class="text-[1.2rem] font-light mb-10 grid place-items-center">
+                        Your flexible solution for research data managing and publishing. The services are designed to make data management effortless and
+                        collaborative, so you can focus on what matters most: your research.
+                        <div class="pl-6">
+                            Learn more:
+                            <ExternalLink link="https://github.com/Sciebo-RDS" text="GitHub repository" />
+                        </div>
+                        <img src="@assets/img/rds-steps-web.png" class="justify-self-center mt-[10rem]" />
+                    </div>
                 </div>
-
-                <div class="flex items-center gap-4 pt-2 place-self-center">
-                    <Card :pt="{ root: 'bg-slate-200 text-center w-64 h-48', content: 'pt-1 place-self-center' }">
-                        <template #title>Step 1</template>
+                <!-- ... -->
+                <div class="grid place-items-center space-y-7">
+                    <img src="@assets/img/image.png" class="w-[25rem]" />
+                    <Card class="w-[30rem]">
+                        <template #title><div class="">Here's how we support you</div></template>
                         <template #content>
-                            <span class="!text-4xl material-icons-outlined mi-add-circle-outline pb-2" />
-                            <p class="flex items-center gap-1 r-text-gray">Create new project</p>
-                        </template>
-                    </Card>
-
-                    <Card :pt="{ root: 'bg-slate-200 text-center w-64 h-48', content: 'pt-1 place-self-center' }">
-                        <template #title>Step 2</template>
-                        <template #content>
-                            <span class="!text-4xl material-icons-outlined mi-file-copy pb-2" />
-                            <p class="flex items-center gap-1 r-text-gray">Collect &amp; manage data</p>
-                        </template>
-                    </Card>
-
-                    <Card :pt="{ root: 'bg-slate-200 text-center w-64 h-48', content: 'pt-1 place-self-center' }">
-                        <template #title>Step 3</template>
-                        <template #content>
-                            <span class="!text-4xl material-icons-outlined mi-edit pb-2" />
-                            <p class="flex items-center gap-1 r-text-gray">Edit metadata &amp; annotations</p>
-                        </template>
-                    </Card>
-
-                    <Card :pt="{ root: 'bg-slate-200 text-center w-64 h-48', content: 'pt-1 place-self-center' }">
-                        <template #title>Step 4</template>
-                        <template #content>
-                            <span class="!text-4xl material-icons-outlined mi-rocket-launch pb-2" />
-                            <p class="flex items-center gap-1 r-text-gray">Finish project &amp; submit data</p>
+                            <ul class="pl-6 list-outside list-disc">
+                                <li class="">
+                                    <strong>Configure research processes -</strong> A research data tool in your cloud that flexibly adapts to your individual
+                                    research workflows.
+                                </li>
+                                <li class="">
+                                    <strong>Project Workflow Integration -</strong> A research data tool that connects existing research data services according
+                                    to your needs
+                                </li>
+                                <li class="">
+                                    <strong>User Control -</strong> Full control over your data, with clear options to manage access, set metadata and
+                                    conditions for sharing.
+                                </li>
+                                <li class="">
+                                    <strong>Ease of Use -</strong> Intuitive services that integrate smoothly into your daily research activities, saving you
+                                    time and effort.
+                                </li>
+                            </ul>
                         </template>
                     </Card>
                 </div>
