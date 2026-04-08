2041adf2af4aabbe0f41a8c3926e644730b9c27f
Daniel Müller <daniel.mueller@uni-muenster.de>
2026-04-08 13:05:10 +0200

    api key header

--- src/server/endpoints/api_endpoints.py
+++ src/server/endpoints/api_endpoints.py
@@ -1,6 +1,6 @@
 import typing
 
-from flask import request
+from flask import abort, request
 
 from common.py.endpoints.endpoint import Endpoint
 
@@ -8,6 +8,11 @@ from common.py.endpoints.endpoint import Endpoint
 def api_v1_ep() -> Endpoint:
     # The main API POST endpoint (/api/v1) accepts and processes messages in plain JSON format
     def _handler() -> typing.Any:
+        from .utils import verify_request_api_header
+
+        # Using this EP always requires a valid API key passed via header, even if the message itself isn't protected
+        verify_request_api_header()
+
         request_data = request.get_json()
         return {"nanu": request_data}
 
---
+++ src/server/endpoints/utils.py
@@ -0,0 +1,17 @@
+from flask import abort, request
+
+from common.py.settings import NetworkSettingIDs
+
+from ..component import ServerComponent
+
+api_key_header = "X-RDS-NG-API-Key"
+
+
+def verify_request_api_header() -> None:
+    comp = ServerComponent.instance()
+
+    if api_key_header not in request.headers:
+        abort(400, "API key header missing")
+    api_key = request.headers[api_key_header]
+    if api_key != comp.data.config.value(NetworkSettingIDs.API_KEY):
+        abort(400, "API key mismatch")
