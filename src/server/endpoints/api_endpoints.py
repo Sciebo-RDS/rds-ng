6f82d7042ea075d439d61c20f876d8ab4129d502
Daniel Müller <daniel.mueller@uni-muenster.de>
2026-04-09 10:28:09 +0200

    add fixed target

--- src/server/endpoints/api_endpoints.py
+++ src/server/endpoints/api_endpoints.py
@@ -1,19 +1,31 @@
 import typing
 
-from flask import abort, request
+from flask import request
 
+from common.py.core.messaging import Channel
 from common.py.endpoints.endpoint import Endpoint
+
+from ..component import ServerComponent
 
 
 def api_v1_ep() -> Endpoint:
     # The main API POST endpoint (/api/v1) accepts and processes messages in plain JSON format
     def _handler() -> typing.Any:
-        from .utils import verify_request_api_header
+        from .utils import verify_request_api_header, abort_request
 
         # Using this EP always requires a valid API key passed via header, even if the message itself isn't protected
         verify_request_api_header()
 
-        request_data = request.get_json()
+        request_data = request.get_json(silent=True)
+
+        if request_data is not None:
+            comp = ServerComponent.instance()
+
+            # Always target the server to prevent unintended routing
+            request_data["target"] = str(Channel.direct(comp.data.comp_id))
+        else:
+            abort_request("Invalid data provided")
+
         return {"nanu": request_data}
 
     return Endpoint(name="api_v1", path="/api/v1", handler=_handler, methods=["POST"])
